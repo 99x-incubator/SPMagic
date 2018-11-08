af9253e4ef5f70a9a74ab7cce969700497a84de1
Sandun Isuru Niraj <SandunI@99x.com>
2018-11-08 16:35:00 +0530

    First Code Push

---
+++ App.js
@@ -0,0 +1,40 @@
+﻿
+$(function () {
+    "use strict";
+
+    var appUrl = GetUrlKeyValue("SPAppWebUrl");
+    var hostUrl = GetUrlKeyValue("SPHostUrl");
+
+	var list = SPMagic.ListManager(appUrl, hostUrl, "Users");
+
+
+	list.getListItemEntityTypeFullName().then(function (res) {
+
+		var listTypeName = res.d.ListItemEntityTypeFullName;
+
+		UpdateFormDigest(_spPageContextInfo.webServerRelativeUrl, _spFormDigestRefreshInterval);
+
+		var user = {
+			"__metadata": { type: listTypeName },
+			"Title": "Malith",
+			"Role": "Admin",
+			"Description": "Hello World",
+			"Email": "malith@99x.lk",
+			"UserID": 4
+		}
+
+		var data = JSON.stringify(user);
+
+		list.createListItem(data, jQuery("#__REQUESTDIGEST").val()).then(function (res) {
+
+			console.log(res);
+		}, function (err) {
+
+			console.log(err);
+
+			});
+
+	});
+	
+
+});
---
+++ README.md
@@ -0,0 +1,12 @@
+# SPMagic - Sharepoint Hosted Addin Helper
+
+This Library is helpful when you developing the Sharepoint Hosted Addin inside the Sharepoint 2013, 2016 or Sharepoint Online. All necessary functions and operations are bundled into callback requests apart from the traditional Ajax calls. 
+### Pre-requisites  
+
+ - jQuery - You can use inbuilt jQuery library inside this repo (Recommended)
+
+### Installation
+Not defined yet
+
+### How to use
+Not defined yet
